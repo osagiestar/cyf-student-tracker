9ef5bf7f7f848756ca03908255a3753be166e668
ISTANBULBEKLE <ekipkalir@gmail.com>
2020-11-22 19:16:09 +0000

    PDSkillsComponent is created

---
+++ src/components/PDSkillsComponent/PDSkillsComponent.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './headingpagecomponent.scss';
+
+export default function PDSkillsComponent (){
+
+    return (
+        <div>
+            <h1>
+                
+            </h1>
+        </div>
+
+    );
+};
